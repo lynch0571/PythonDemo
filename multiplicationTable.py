4df1c4165fc033a710dda3156d1ab219c663c76e
linqi <lynch0571@gmail.com>
2016-08-07 09:11:01 +0800

    Simplify the Code

--- multiplicationTable.py
+++ multiplicationTable.py
@@ -1,15 +1,13 @@
 def printMultiplicationTableFunA():
     for x in range(1,10):
         for y in range(x,10):
-            result=" "+str(x*y) if x*y<10 else str(x*y)
-            print(str(x)+"*"+str(y)+"="+result,end="    ")
+            print('{0}*{1}={2}'.format(x,y,'{: >2}'.format(x*y)),end="    ")
         print("")
         
 def printMultiplicationTableFunB():
     for x in range(1,10):
         for y in range(1,x+1):
-            result=" "+str(x*y) if x*y<10 else str(x*y)
-            print(str(y)+"*"+str(x)+"="+result,end="    ")
+            print('{0}*{1}={2}'.format(y,x,'{: >2}'.format(x*y)),end="    ")
         print("")
         
 printMultiplicationTableFunB()
